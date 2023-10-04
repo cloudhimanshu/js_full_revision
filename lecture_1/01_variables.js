59228af6e5196cb4e91d9083f87f60718d17726b
Himanshu kaushik <rtxhoney@duck.com>
2023-10-05 00:38:59 +0530

    added notes

--- lecture_1/01_variables.js
+++ lecture_1/01_variables.js
@@ -2,7 +2,7 @@ const accountId = 144553;
 let accountEmail  = "rthoney@google.com"
 var accountPassword ="121213"
 accountCity = "chandigarh"
-
+let himanshu;
 // accountId = 12
 
 
@@ -10,8 +10,12 @@ accountCity = "chandigarh"
 accountEmail = "hc@hc.com"
 accountPassword = "1213131"
 accountCity = "bangluru"
+/*
+prefer not to use var
+because of issue in block scope and functional scope
 
+*/
 
 console.log(accountId);
 
-console.table([accountId,accountEmail,accountPassword,accountCity])
+console.table([accountId,accountEmail,accountPassword,accountCity , himanshu ])
