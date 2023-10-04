e4db526be5d4af20afa71a635ba7d95ea794fe2f
Himanshu kaushik <rtxhoney@duck.com>
2023-10-05 00:30:17 +0530

    new

---
+++ lecture_1/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553;
+let accountEmail  = "rthoney@google.com"
+var accountPassword ="121213"
+accountCity = "chandigarh"
+
+// accountId = 12
+
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "1213131"
+accountCity = "bangluru"
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
