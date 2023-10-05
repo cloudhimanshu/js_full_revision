a1937f8173f2d21dfe47e9ee142c999dca4fd0e7
Himanshu kaushik <rtxhoney@duck.com>
2023-10-05 17:09:23 +0530

    data types

---
+++ lecture_1/02_dataTypes.js
@@ -0,0 +1,27 @@
+//code readability should be top notch before start writing code keep this thing in mind !! <<<Happy Coding>>> :)
+
+"use strict"; //Treat all js code as a newer version
+//aleart statement - we are using node js not browser so it will throw error!
+// alert("Hi himanshu How are You? :) ")
+
+console.log(2+2);
+console.log("himanshu");
+
+const himanshu = null;
+let state;
+let name = "himanshu";
+let age = 18;
+let isLoggedIn = false;
+
+// PREMITIVE DATA TYPES
+
+//number => 2 to power 53
+// big int example of usage is stock market etc it makke big numbers
+// string => ""
+// boolean => true/false
+// null it is a standalone value it is a type
+// undefined 
+// symbol => unique
+
+// object 
+console.log(typeof(null)); //object
